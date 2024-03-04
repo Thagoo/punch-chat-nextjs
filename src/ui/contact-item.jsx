204ae3c4f0ffed4d32178859cc8ceb9812f55bbf
thagoo <lohitgowda56@gmail.com>
2024-03-04 20:21:03 +0530

    punch: rename component of contact-items

--- src/ui/contact-item.jsx
+++ src/ui/contact-item.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
-export default function Contacts({ contactUser }) {
+export default function ContactItem({ conversation, targetUserDetails }) {
   const pathname = usePathname();
   const { data: session } = useSession();
 
@@ -12,32 +12,40 @@ export default function Contacts({ contactUser }) {
     <>
       <div
         className={`rounded-sm py-2 text-sm hover:bg-gray-300 ${
-          pathname.includes(contactUser.id) ? "bg-gray-300" : "bg-white"
+          pathname.includes(targetUserDetails?._id) ? "bg-gray-300" : "bg-white"
         }`}
       >
         <li>
-          <Link href={`/chat/${contactUser.id}`}>
+          <Link href={`/chat/${targetUserDetails?._id}`}>
             <div className="flex items-center">
               <Image
-                src={contactUser.avatar || "/assets/no-avatar.svg"}
+                src={targetUserDetails?.avatar || "/assets/no-avatar.svg"}
                 alt="avatar logo"
-                width={50}
+                width={60}
                 height={24}
                 className="rounded-full p-1 mr-2"
               />
               <div className="flex flex-col flex-grow">
                 <p className="text-lg ">
-                  {session?.user.id == contactUser.id
+                  {session?.user.id == targetUserDetails?._id
                     ? "You"
-                    : contactUser.firstName}
+                    : targetUserDetails?.firstName}
                 </p>
-                <p className="text-xs text-slate-800">
-                  @{contactUser.username}
+                {conversation ? (
+                  ""
+                ) : (
+                  <p className="text-sm text-slate-600">
+                    @{targetUserDetails?.username}
+                  </p>
+                )}
+                <p className="text-md text-slate-500">
+                  {conversation?.lastMessage}
                 </p>
               </div>
+
               <span
                 class={`flex w-3 h-3 me-3 ${
-                  contactUser.isActive ? "bg-green-500" : "bg-red-500"
+                  targetUserDetails?.isActive ? "bg-green-500" : "bg-red-500"
                 } rounded-full`}
               ></span>
             </div>
